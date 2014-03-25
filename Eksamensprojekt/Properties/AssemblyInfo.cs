9a9f5f5a4ceb5d44eab2beb319a612681a156647
kiljacken <mine809@gmail.com>
2014-03-25 14:23:41 +0100

    Basic datastructure for storing quizzes

--- Eksamensprojekt/Properties/AssemblyInfo.cs
+++ Eksamensprojekt/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Eksamensprojekt")]
+[assembly: AssemblyTitle("FlashQuiz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Eksamensprojekt")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("FlashQuiz")]
+[assembly: AssemblyCopyright("Copyright ©  Emil Lauridsen 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ Eksamensprojekt/Quiz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Eksamensprojekt
+{
+    [Serializable()]
+    class Quiz
+    {
+        public String Name;
+        public String Description;
+        public IList<QuestionReference> QuestionReferences;
+        public IList<ImageReference> ImageReferences;
+
+        public static Quiz LoadFromZip(String path)
+        {
+            ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Read);
+            ZipArchiveEntry quizEntry = archive.GetEntry("quiz.xml");
+
+            Quiz quiz = null;
+            using (Stream stream = quizEntry.Open())
+            {
+                XmlSerializer x = new XmlSerializer(typeof(Quiz));
+                quiz = (Quiz)x.Deserialize(stream);
+            }
+
+            return quiz;
+        }
+
+    }
+
+    [Serializable()]
+    class QuestionReference
+    {
+        public String Path;
+    }
+
+    [Serializable()]
+    class ImageReference
+    {
+        public String Path;
+    }
+}
